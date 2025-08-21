ae3958f40cb23d0f48568928a2420671f2268dff
Maurice Miema <159928035+Maurice-Miema@users.noreply.github.com>
2025-08-21 07:27:50 +0200

    Update Vehicle.routes.js

--- server/routes/Vehicle.routes.js
+++ server/routes/Vehicle.routes.js
@@ -8,7 +8,7 @@ router.post("/entreeVehicule", verifyToken, vehicleEntree);
 router.post("/sortieVehicule", verifyToken, vehicleSortie);
 router.get("/getAllVehicles", verifyToken, getAllVehicles);
 router.get("/getStats", verifyToken, getStats);
-router.delete("/deleteVehicle", verifyToken, deleteVehicle);
+router.delete("/deleteVehicle/:id", verifyToken, deleteVehicle);
 router.get("/searchByPlaque", verifyToken, searchByPlaque);
 
 module.exports = router;
